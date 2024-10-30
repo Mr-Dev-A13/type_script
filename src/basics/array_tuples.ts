33e5c3e381c9d4b008806f45ab7aaefe284f08de
Mr-Dev-A13 <avik.ghukasyan13@gmail.com>
2024-10-30 23:42:47 +0400

    feat(basics): add array and tuple types for basics

---
+++ src/basics/array_tuples.ts
@@ -0,0 +1,16 @@
+// Array
+let arr: number[] = [1, 2, 3];
+
+// Usage
+arr = [1];
+arr = [1, 2, 3, 4, 5];
+// arr = ["hello"]; //error;
+
+// Tuple
+let tuple: [number, number] = [1, 2];
+
+// Usage
+tuple = [1, 2];
+tuple = [3, 4];
+tuple = [5, 6, 7]; //error: must be 2 items
+tuple = [8]; //error: must be 2 items
