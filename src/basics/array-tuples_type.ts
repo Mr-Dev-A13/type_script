50afade49058ee138260ef092d93d38351b426aa
Mr-Dev-A13 <avik.ghukasyan13@gmail.com>
2024-10-31 09:48:21 +0400

    feat(basics): add object and aliases types for basics

--- src/basics/array-tuples_type.ts
+++ src/basics/array-tuples_type.ts
@@ -12,5 +12,6 @@ let tuple: [number, number] = [1, 2];
 // Usage
 tuple = [1, 2];
 tuple = [3, 4];
-tuple = [5, 6, 7]; //error: must be 2 items
-tuple = [8]; //error: must be 2 items
+// tuple = [5, 6, 7]; //error: must be 2 items
+// tuple = [8]; //error: must be 2 items
+// tuple = ["atom", 643]; //error: must be number
