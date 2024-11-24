4fac5e7a8b947c46cf0144ea56ff14f853dfec06
Mr-Dev-A13 <avik.ghukasyan13@gmail.com>
2024-11-24 16:31:08 +0400

    feat(basics): add type assertions for basic

---
+++ src/basics/assertions-type.ts
@@ -0,0 +1,9 @@
+let nameVariable = "Pol";
+
+if (nameVariable === "string") {
+    const otherNameVariable = nameVariable.trim();
+}
+
+// or
+
+const otherNameVariable = (nameVariable as string).trim();
